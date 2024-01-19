2cd8f72a32ef8cc3fb7d2c70f5108fa98df004dc
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2024-01-18 21:01:24 -0600

    Added step 1 to process simple json

--- src/test/java/JsonParserTests.java
+++ src/test/java/JsonParserTests.java
@@ -1,5 +1,6 @@
 package src.test.java;
 
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,7 +9,10 @@ import src.main.java.JsonParser;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
 public class JsonParserTests{
     private JsonParser jsonParser;
@@ -52,32 +56,42 @@ public class JsonParserTests{
     }
 
     @Test
-    public void parseStringSingleKeyValueJson() throws IOException {
-        String jsonString = "{\"key\":\"value\"}";
+    public void parseStringMultiKeyValueJson() throws IOException {
+        Random random = new Random();
+        int keyValuePairs = random.nextInt(20);
+        Map<String, String> json = generateRandomJsonWithNStringKeyValues(keyValuePairs);
+        String jsonString = new Gson().toJson(json);
         InputStream inputStream = new ByteArrayInputStream(jsonString.getBytes());
         Map<String, Object> parsedJson = jsonParser.parse(inputStream);
         Assert.assertNotNull(parsedJson);
-        Assert.assertEquals(1, parsedJson.size());
-        Object value = parsedJson.get("key");
-        Assert.assertNotNull(value);
-        Assert.assertTrue(value instanceof String);
-        Assert.assertEquals("value", value);
+        Assert.assertEquals(keyValuePairs, parsedJson.size());
+        for(String key : json.keySet()){
+            Object value = parsedJson.get(key);
+            Assert.assertNotNull(value);
+            Assert.assertTrue(value instanceof String);
+            Assert.assertEquals(json.get(key), value);
+        }
     }
 
     @Test
-    public void parseStringMultiKeyValueJson() throws IOException {
-        String jsonString = "{\"key1\":\"value1\", \"key2\":\"value2\"}";
+    public void failToParseInvalidJsonString() throws IOException {
+        String jsonString = "{\"key\":\"value\",}";
         InputStream inputStream = new ByteArrayInputStream(jsonString.getBytes());
         Map<String, Object> parsedJson = jsonParser.parse(inputStream);
-        Assert.assertNotNull(parsedJson);
-        Assert.assertEquals(2, parsedJson.size());
-        Object value1 = parsedJson.get("key1");
-        Assert.assertNotNull(value1);
-        Assert.assertTrue(value1 instanceof String);
-        Assert.assertEquals("value1", value1);
-        Object value2 = parsedJson.get("key2");
-        Assert.assertNotNull(value2);
-        Assert.assertTrue(value2 instanceof String);
-        Assert.assertEquals("value2", value2);
+        System.out.println(" Hello");
+    }
+
+    private Map<String, String> generateRandomJsonWithNStringKeyValues(int pairs){
+        Map<String, String> json = new HashMap<>();
+        for(int i = 0 ; i < pairs ; i++){
+            String randomKey;
+            do
+            {
+                randomKey = UUID.randomUUID().toString();
+            }while(json.containsKey(randomKey));
+            String randomValue = UUID.randomUUID().toString();
+            json.put(randomKey, randomValue);
+        }
+        return json;
     }
 }
